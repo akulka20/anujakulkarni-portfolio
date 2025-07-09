234d611aa13b298e1d2bbf05860d4e36973a1b0d
akulka20 <kanuja94@gmail.com>
2025-07-09 22:05:57 +0530

    Fix: removed system files and updated .gitignore

---
+++ anujakulkarni-portfolio/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-4xl font-bold text-purple-600">Hello, I’m Anuja Kulkarni</h1>
+      <p className="mt-2 text-lg text-gray-700">Welcome to my portfolio website!</p>
+    </main>
+  );
+}
